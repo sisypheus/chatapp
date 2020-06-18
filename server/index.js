6c264950fe3b64396f8042f3f741b311d4a4ad4c
tpoette <theo.poette@epitech.eu>
2020-06-18 19:47:00 +0200

    [+] adding users to the same socket by room id + having the history of messages sent in a room

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const {addUser, removeUser, getUser, getUserRoom} = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,10 +13,29 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('New connection!!!');
+    socket.on('join', ({name, room}, callback) => {
+        const {error, user} = addUser({id: socket.id, name, room});
 
-    socket.on('disconect', () => {
-        console.log('1 user less :(((');
+        if (error)
+            return callback(error);
+
+        socket.emit('message', {user: 'admin', text: name + ', welcome to the room ' + room});
+        socket.broadcast.to(room).emit('message', {user: 'admin', text: name + ', has joined'});
+        socket.join(room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        console.log('1 user less');
     })
 });
 
