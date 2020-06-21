39fcf2ae5c35e0f571a3751379f379ad8f080491
tpoette <theo.poette@epitech.eu>
2020-06-21 12:24:37 +0200

    [+] login form design responsive

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -8,15 +8,17 @@ const Join = () => {
     const [room, setRoom] = useState('');
 
     return (
+        <div className="body">
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
                 <h1 className="heading">Join</h1>
-                <input placeholder="Username" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
-                <input placeholder="Room" className="joinInput" type="text" onChange={(event) => setRoom(event.target.value)} />
+                <p><input placeholder="Username" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></p>
+                <p><input placeholder="Room" className="joinInput" type="text" onChange={(event) => setRoom(event.target.value)} /></p>
                 <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={'/chat?name=' + name + '&room=' + room}>
                     <button className="Button" type="Submit">Sign in</button>
                 </Link>
             </div>
+        </div>
         </div>
     )
 }
