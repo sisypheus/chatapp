171e3eb0fbd962d2b392b29e91f858b7ab8079f9
tpoette <theo.poette@epitech.eu>
2020-11-11 15:05:52 +0100

    [+] changed endpoint to hosted script

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({ location }) => {
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000/'; //https://react-node-realtimechatapp.herokuapp.com/';
+    const ENDPOINT = 'https://react-node-realtimechatapp.herokuapp.com/';
 
     useEffect(() => {
         const {name, room} = queryString.parse(location.search);
